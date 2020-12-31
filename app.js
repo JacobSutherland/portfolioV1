329cbdce09bd05349d3d170a7f204e89f093875d
JacobSutherland <jacobsutherland224@gmail.com>
2020-12-30 18:35:46 -0600

    added mobile responsiveness

--- app.js
+++ app.js
@@ -1,50 +1,51 @@
 const projectDescription = document.querySelectorAll('.projects-container p');
 const projectCard = document.querySelectorAll('.project-img-description');
 const header = document.querySelector('header')
+const headerNav = document.querySelector('header nav')
 const dropDownIcon = document.querySelector('.drop-down')
 const closeIcon = document.querySelector('.close')
+const menuItems = document.querySelectorAll('header nav a')
 
-closeIcon.classList.add('drop-down-closed')
+//Sets the description of projects to none and toggles them on and off based on mouseover and mouseout and screen size
+function toggleDescriptions(){
+    if(screen.width >= 960){
+        console.log(screen.width)
+        for(let i = 0; i < projectDescription.length; i++){
+            projectDescription[i].style.display = 'none';
+        }
 
-function constructMenu(){
-    let menu = `
-    <div class='logo-nav-container'>
-    <h1 class='logo'>Jacob Suther<span>land</span></h1>
-    <nav class='heading-nav'>
-        <a href='#home'>Home</a>
-        <a href='#bio'>About</a>
-        <a href='#projects'>My Work</a>
-        <a href='#blogs'>Blogs</a>
-        <a href="mailto:jacobsutherland224@gmail.com">Contact Me</a>
-        <img class='drop-down' src='assets/dropDown.svg'>
-        <img class='close' src='assets/close.svg'>
-    </nav>
-</div>
-    `
-    header.insertAdjacentHRML('beforeend', menu)
+        for(let i = 0; i < projectCard.length; i++){
+            projectCard[i].addEventListener('mouseover', () => {
+            let currentIndex = i;
+            projectDescription[currentIndex].style.display = 'initial';
+            })
+        }
+
+        for(let i = 0; i < projectCard.length; i++){
+            projectCard[i].addEventListener('mouseout', () => {
+            let currentIndex = i;
+            projectDescription[currentIndex].style.display = 'none';
+            })
+        }
+    }   
 }
 
-dropDownIcon.addEventListener('click', constructMenu)
+closeIcon.style.display = 'none';
+dropDownIcon.addEventListener('click', () => {
+    headerNav.classList.remove('hide-nav');
+    closeIcon.style.display ='block';
+})
+closeIcon.addEventListener('click', () => {
+    headerNav.classList.add('hide-nav')
+})
 
-//Sets the description of projects to none and toggles them on and off based on mouseover and mouseout
-function toggleDescriptions(){
-    for(let i = 0; i < projectDescription.length; i++){
-        projectDescription[i].style.display = 'none';
-    }
-
-    for(let i = 0; i < projectCard.length; i++){
-        projectCard[i].addEventListener('mouseover', () => {
-        let currentIndex = i;
-        projectDescription[currentIndex].style.display = 'initial';
-        })
-    }
-
-    for(let i = 0; i < projectCard.length; i++){
-        projectCard[i].addEventListener('mouseout', () => {
-        let currentIndex = i;
-        projectDescription[currentIndex].style.display = 'none';
+function closeMenu() {
+    for(let i = 0; i < menuItems.length; i++){
+        menuItems[i].addEventListener('click', () => {
+            headerNav.classList.add('hide-nav');
         })
     }
 }
 
 toggleDescriptions();
+closeMenu();
